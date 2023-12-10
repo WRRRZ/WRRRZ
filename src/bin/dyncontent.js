b927ad0bd33cb5ab74ccef46aaf5ce8099e7f85d
stereoIII6 <stereodocbush@gmail.com>
2023-12-10 16:42:47 +0100

    redesign starts now

--- src/bin/dyncontent.js
+++ src/bin/dyncontent.js
@@ -20,16 +20,6 @@ export const develop_stage = `
   <h1 id="" class="title">Create a custom ERC20 Token Contract</h1>
   <div id='' class='txt'></div>
   <img src='' class="img" />
-</div>
-<div id="nftbuild" class="barpost">
-  <h1 id="" class="title">Create a custom dynamic NFT Contract</h1>
-  <div id='' class='txt'></div>
-  <img src='' class="img" />
-</div>
-<div id="sftbuild" class="barpost">
-  <h1 id="" class="title">Create a custom dynamic SFT Contract</h1>
-  <div id='' class='txt'></div>
-  <img src='' class="img" />
 </div>`;
 export const launch_stage = `<div id='showbody'>launch</div>`;
 export const info_stage = `<div id='showbody'>info</div>`;
