3e8e3a8e960dc2cdd9ea53549e16e9c47a18db5e
stereoIII6 <stereodocbush@gmail.com>
2023-12-10 15:46:32 +0100

    big heading

--- src/bin/dyncontent.js
+++ src/bin/dyncontent.js
@@ -7,13 +7,11 @@ Your browser does not support the video tag.
 </div>`;
 export const iii6_stage = `
 <div id='text' class='boxpost1'>
-    <div id='' class='txt'>WRRRZ is a competitive open-world platformer game set in a stylistic, futuristic African village named Kente. The game blends unique fashion elements with combat and strategy, centered around clan-based looting mechanics to acquire valuable fashion items that enhance player abilities.</div>
+    <h1 id='' class='txt'>WRRRZ is a competitive open-world platformer game set in a stylistic, futuristic African village named Kente. The game blends unique fashion elements with combat and strategy, centered around clan-based looting mechanics to acquire valuable fashion items that enhance player abilities.</h1>
 </div>`;
 export const design_stage = `
 <div id='desdyn' class='boxpost1'>
     <h1 id='' class='title'>Codebender Dynamic NFTs Editor</h1>
-    <div id='' class='txt'></div>
-    <img src='' class='img' />
 </div>
 <div id='desdapps' class='boxpost2'>
     <h1 id='' class='title'>Interactive dApp SDK</h1>
