82db4d49597bc96873ddc32d72d0e6391cb5b0fe
stereoIII6 <stereodocbush@gmail.com>
2023-12-11 01:32:54 +0100

    clip switch

--- src/bin/dyncontent.js
+++ src/bin/dyncontent.js
@@ -46,7 +46,7 @@ The Rhodium Coin is the main InGame Currency of WRRRZ ...
 </div>
 <div id='rhodium' class="boxpost2">
 <video width="100%" autoplay loop >
-  <source src="./images/clip2.mp4" type="video/mp4">
+  <source src="./images/clip4.mp4" type="video/mp4">
 Your browser does not support the video tag.
 </video>
 </div>`;
